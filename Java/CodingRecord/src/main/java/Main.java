e96c00f7af54a50917ff417feeea025a65d59f4a
Park90 <jmp1533@gmail.com>
2022-08-18 19:15:22 +0900

    Java 코딩연습 풀이

--- Java/CodingRecord/src/main/java/Main.java
+++ Java/CodingRecord/src/main/java/Main.java
@@ -1,11 +1,15 @@
-
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args){
         String result = "";
 
+        HashSet<Integer> list = new HashSet<>();
 
+        list.add(1);
+        list.add(3);
 
-        System.out.println(result);
+        System.out.println(list.size());
     }
 }
