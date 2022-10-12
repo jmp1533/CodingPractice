be09b9f09885eee961c6c87a1cb760fae0cc803e
Park90 <jmp1533@gmail.com>
2022-10-12 21:46:42 +0900

    [프로그래머스] 제곱수 판별하기

---
+++ Java/CodingRecord/src/main/java/programmers/solution022.java
@@ -0,0 +1,23 @@
+package programmers;
+/*
+문제 설명
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때,
+n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 1,000,000
+*/
+
+import java.util.*;
+public class solution022 {
+    public int solution(int n) {
+        double sqrt = Math.sqrt(n);
+
+        if(sqrt % 1 == 0){
+            return 1;
+        }else{
+            return 2;
+        }
+
+    }
+}
